651b62bdf0c886cad18d5a474de0a245f3df0199
Miika Sirén <miika.siren@cs.helsinki.fi>
2013-11-08 16:48:52 +0200

    improved min max completed

--- week5/minmax-megahot.sh
+++ week5/minmax-megahot.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-find $1 -name hp-temps.txt -exec grep "PROCESSOR_ZONE" {} + -exec sed 's|[ /]\+|\t|g' + -exec sort -nuk5 \;
-#{} \; -exec sed -i '/./d' {} \;
-#find /home/mssiren/Downloads/lost24/monitor -name hp-temps.txt -exec grep "PROCESSOR_ZONE" {} +
+max=-1000
+min=1000
+
+#replace empty spaces and slashes with tabs
+#grep the rest of the line starting from the timestamp yyyy.mm.dd
+#replace the C from the temperatures with nothing
+
+time find $1 -name hp-temps.txt -exec grep "PROCESSOR_ZONE" {} + | sed 's|[ /]\+|\t|g' | grep -o "[0-9]\{4\}\.[0-9]\{2\}\.[0-9]\{2\}.*" | sed 's|\C||g' |
+
+while read yymmdd hhmm _ _ temperature _ _; do
+	if [[ "$max" -lt "$temperature" ]]; then
+			max="$temperature"
+			echo "NEW MAX:" "$yymmdd""$hhmm" "$max"
+	fi
+	
+	if [[ "$temperature" -lt "$min" ]]; then
+			min="$temperature"
+			echo "NEW MIN:" "$yymmdd""$hhmm" "$min"
+	fi
+done
